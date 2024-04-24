48df05dc77807c76a418d619bf9161b46c817e11
Oleg <Chehchekhovsky@gmail.com>
2024-04-24 20:25:14 +0300

    changed urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,23 +2,22 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from issues.api import (IssuesAPI, IssuesRetrieveUpdateDeleteAPI, issues_close,
-                        issues_take, messages_api_dispatcher)
+from issues.api import (# noqa
+    IssuesAPI,# noqa
+    IssuesRetrieveUpdateDeleteAPI,# noqa
+    issues_close,# noqa
+    issues_take,# noqa
+    messages_api_dispatcher,# noqa
+)# noqa
 from users.api import UserListCreateAPI
 
 urlpatterns = [
-    # admin
     path("admin/", admin.site.urls),
-    # users
     path("users/", UserListCreateAPI.as_view()),
-    # issues
     path("issues/", IssuesAPI.as_view()),
     path("issues/<int:id>", IssuesRetrieveUpdateDeleteAPI.as_view()),
     path("issues/<int:id>/close", issues_close),
     path("issues/<int:id>/take", issues_take),
-    # messages
     path("issues/<int:issue_ad>/messages", messages_api_dispatcher),
-    # Authentication
-    # path("auth/token/", token_obtain_pair),
     path("auth/token/", TokenObtainPairView.as_view()),
 ]
